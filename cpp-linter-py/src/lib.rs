7169129b50483501f228b112625d220b0006df8d
Brendan <2bndy5@gmail.com>
2024-08-13 14:06:11 -0700

    bump deps

--- cpp-linter-py/src/lib.rs
+++ cpp-linter-py/src/lib.rs
@@ -12,9 +12,9 @@ fn main(args: Vec<String>) -> PyResult<i32> {
 /// ``cpp_linter.run`` whose only exposed function is used as the entrypoint script.
 /// See the pure python sources in this repo's cpp_linter folder (located at repo root).
 #[pymodule]
-fn cpp_linter(_py: Python, m: &PyModule) -> PyResult<()> {
-    let run_submodule = PyModule::new(_py, "run")?;
+fn cpp_linter(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    let run_submodule = PyModule::new_bound(m.py(), "run")?;
     run_submodule.add_function(wrap_pyfunction!(main, m)?)?;
-    m.add_submodule(run_submodule)?;
+    m.add_submodule(&run_submodule)?;
     Ok(())
 }
