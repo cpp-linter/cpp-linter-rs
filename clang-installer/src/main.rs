bdf2558f979da229d547bc54f912221cf551d83a
Brendan <2bndy5@gmail.com>
2026-06-06 23:40:34 -0700

    allow verbose logs in clang-tools binary

--- clang-installer/src/main.rs
+++ clang-installer/src/main.rs
@@ -99,14 +99,26 @@ mod logging {
 pub struct CliOptions {
     /// The desired version of clang to install.
     #[arg(
-            short,
-            long,
-            default_missing_value = "CPP-LINTER-VERSION",
-            num_args = 0..=1,
-            value_parser = RequestedVersion::from_str,
-            default_value = "",
+        short,
+        long,
+        default_missing_value = "CPP-LINTER-VERSION",
+        num_args = 0..=1,
+        value_parser = RequestedVersion::from_str,
+        default_value = "",
     )]
     pub version: Option<RequestedVersion>,
+
+    /// Enable verbose logging for debugging purposes.
+    ///
+    /// This will include more DEBUG level log messages.
+    /// Without it, log level is set to INFO by default.
+    #[arg(
+        short = 'V',
+        long,
+        default_value_t = false,
+        action = clap::ArgAction::SetTrue,
+    )]
+    pub verbose: bool,
 
     /// The clang tool to install.
     #[arg(
@@ -132,7 +144,9 @@ pub struct CliOptions {
 async fn main() -> Result<()> {
     logging::initialize_logger();
     let options = CliOptions::parse();
-
+    if options.verbose {
+        log::set_max_level(log::LevelFilter::Debug);
+    }
     let tool = options
         .tool
         .expect("--tool should have a default value: [clang-format, clang-tidy]");
